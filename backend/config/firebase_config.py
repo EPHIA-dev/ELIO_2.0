e095391810c7e6697475795b09a2cc5b49fad87d
Mathieu <acaletm@gmail.com>
2025-02-08 10:26:18 +0100

    FIX FDP

--- backend/config/firebase_config.py
+++ backend/config/firebase_config.py
@@ -1,7 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials, auth
+import os
 
 # Initialisez Firebase Admin avec vos credentials
 # Vous devez télécharger le fichier JSON des credentials depuis la console Firebase
-cred = credentials.Certificate("config/elio-49720-firebase-adminsdk-fbsvc-735bfdc0df.json")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+cred_path = os.path.join(current_dir, "elio-49720-firebase-adminsdk-fbsvc-735bfdc0df.json")
+cred = credentials.Certificate(cred_path)
 firebase_admin = firebase_admin.initialize_app(cred) 
